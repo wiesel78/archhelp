d1b7c027a31e02fe066f17924a051343bb61eedc
wiesel <wurst78@gmail.com>
2012-12-08 22:42:17 +0100

    headers

---
+++ .header/perl.pl
@@ -0,0 +1,69 @@
+sub progSearch{
+    my $program = shift @_;
+    my $_ = shift @_;
+    
+    foreach ($_){
+        if(/\/$program\s/ or /\($program\)/){
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+
+sub progIsInstall{
+    my $program = shift @_;
+    my $_ = `pacman -Qs $program`;
+    
+    return progSearch($program, $_);
+}
+
+sub progExist{
+    my $program = shift @_;
+    my $_ = `pacman -Ss $program`;
+    
+    return progSearch($program, $_);
+}
+
+sub isRoot{
+    chomp( my $user = `id -nu` );
+    
+    return 1 if $user eq "root";
+    return 0 unless $user eq "root";
+}
+
+sub progInstall{
+    my $program = shift @_ or die "pacman need a programname";
+    
+    if(progIsInstall($program)){
+        return 1;
+    }elsif(progExist($program)){
+        if(isRoot){
+            my $_ = `pacman -S $program --noconfirm`;
+            return 1;
+        }elsif(progIsInstall("sudo")){
+            my $_ = `sudo pacman -S $program --noconfirm`;
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+
+sub progRemove{
+    my $program = shift @_ or die "pacman need a programname";
+    
+    if(progIsInstall($program)){
+        if(isRoot){
+            my $_ = `pacman -R $program --noconfirm`;
+            return 1;
+        }elsif(progIsInstall("sudo")){
+            my $_ = `sudo pacman -R $program --noconfirm`;
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+
+1;
