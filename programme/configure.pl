e67005dc2d50198b337fe19e32fd357fe33cacc1
wiesel <wurst78@gmail.com>
2012-12-12 00:18:49 +0100

    programmlisten generator

---
+++ programme/configure.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+use v5.16;
+
+my %groups;
+my $groupname = "none";
+my $output = "";
+
+foreach (<>){
+    chomp;
+    
+    if( /\s*#.*/ ){
+        
+    }elsif( /\[(.*)\]/){
+        my @progs;
+        $groupname = $1;
+        $groups{$groupname} = \@progs;
+    }elsif( /\s*(.*)\s*#*.*/ ){
+        push @{$groups{$groupname}}, $1;
+    }
+}
+
+$output .= "all: ";
+foreach (keys %groups){
+    $output .= "$_ ";
+}
+
+
+while (my ($key, $value) = each %groups){
+    $output .= "\n\n";
+
+    $output .= "$key : \n";
+    $output .= "	pacman -S ";
+    
+    foreach (@{$value}){
+        $output .= "$_ ";
+    }
+}
+
+say $output;
