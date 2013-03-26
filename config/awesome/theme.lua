41f461cad05cf922c9841dd0d370fe0b19ad3420
wiesel <wurst78@gmail.com>
2013-03-26 23:37:54 +0100

    theme.lua

--- config/awesome/theme.lua
+++ config/awesome/theme.lua
@@ -72,7 +72,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/home/wiesel/Bilder/Wallpaper/Sexy/bestof/wallpaper.jpg"
+theme.wallpaper = "/home/wiesel/.config/awesome/wallpaper.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
