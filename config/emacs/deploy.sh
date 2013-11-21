9be3e60088176f8c988c3bacd67fe1260757d7a1
Henrik <juerges.henrik@gmail.com>
2013-11-21 19:24:01 +0100

    Doofen Fehler behoben

--- config/emacs/deploy.sh
+++ config/emacs/deploy.sh
@@ -1,8 +1,8 @@
 # deploys emacs configuration with prelude
 CONFIG=${HOME}/.emacs.d
 
-# save existing one
-tar -cf ${HOME}/emacs.tar.gz ${HOME}/.emacs ${HOME}/.emacs.d
+# save existing one /blödsinn
+#tar -cf ${HOME}/emacs.tar.gz ${HOME}/.emacs ${HOME}/.emacs.d
 # if not create dir
 mkdir -p $(CONFIG)/.emacs.d
 # install prelude
@@ -10,4 +10,4 @@ export PRELUDE_INSTALL_DIR="${CONFIG}"
 curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
 # overwrite existing configuration
 cp -f ./prelude-modules.el ${CONFIG}/prelude-modules.el
-cp -rf ./personal ${CONFIG}/personal
+cp -rf ./personal ${CONFIG}/personal/*
