e7ad0cc54f964d71ef464a5886aa1209202a743f
Henrik Jürges <juerges.henrik@gmail.com>
2014-03-17 14:41:50 +0100

    emacs conf angepasst

--- config/emacs/deploy.sh
+++ config/emacs/deploy.sh
@@ -1,11 +1,13 @@
-# deploys emacs configuration with prelude
+# deploy emacs configuration with prelude
 CONFIG=${HOME}/.emacs.d
+CWD=`pwd`
 
-# if not create dir
-mkdir -p $(CONFIG)/.emacs.d
 # install prelude
 export PRELUDE_INSTALL_DIR="${CONFIG}"
 curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
+
 # overwrite existing configuration
-cp -f ./prelude-modules.el ${CONFIG}/prelude-modules.el
-cp -rf ./personel/ ${CONFIG}/
+cd $CWD
+cp -f prelude-modules.el ${CONFIG}/prelude-modules.el
+cp -rf personal/ ${CONFIG}/
+echo "Config successfully copied."
