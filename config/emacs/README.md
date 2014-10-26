67a3f8a21b8f56be7be6afd8b1d75becd2d24b6c
santifa <juerges.henrik@gmail.com>
2014-10-26 02:37:42 +0200

    switched to graphene and custom config for better defaults, layouts and a smaller configuration

--- config/emacs/README.md
+++ config/emacs/README.md
@@ -1,31 +1,5 @@
 # Emacs
 
-Provided with:
-..[Prelude](http://batsov.com/prelude/) ,
-..[God-Mode](https://github.com/chrisdone/god-mode)
-..[Dark-Blue Theme](https://github.com/emacs-jp/replace-colorthemes/blob/master/dark-blue-theme.el)
-and some other configurations. Check `personel/`
-
-## Usage
-
-`make fresh-client` Installs Prelude on a new client.
-`make to-client` Copies this configuration to `~/.emacs.d/personal/`
-
 If you're using systemd as init system you can use the `emacs@service` file to load emacs
 within your system boot. This file goes to `/usr/lib/systemd/system`. After that 
 you can [enable|start] it with `systemctl [enable|start] emacs@<username>`.
-
-Erweiterungen
-* `web-mode, lua-mode, autocomplete, sr-speedbar ` sind als Zusatzmodule definiert (`autocomplete` ist deaktiviert)
-* `C-x C-v` - öffne Datei mit sudo
-* Zeilennummern aktiviert
-* `Shift - ArrowKeys` - zum Wechseln der Fenster
-* Cut and Paste aktiviert
-* UTF-8 als Standard Encoding
-* `C-c n` - Räumt den aktuellen Buffer auf (Whitespaces und Tabs)
-* smex aktiviert
-* `M-i` - Go to symbol
-* `C-x C-t` - Insert Timestamp
-* `C-x k` - Kill current buffer
-* `C-c b` - sr-speedbar-toogle
-* `C-q` - auto-complete trigger
---
+++ config/emacs/_henrik/init.el
@@ -0,0 +1,125 @@
+;;; Init.el --- Initial Emacs startup script
+
+;;; Commentary:
+;;; This is my initial startup script for emacs.
+;;; It uses graphene as a base for sane defaults.
+
+;;; Code:
+;; Add user information
+(setq user-full-name "Henrik Jürges")
+(setq user-mail-address "juerges.henrik@gmail.com")
+
+;; Set PATH
+(setenv "PATH" (concat "~/.cabal/bin:" (getenv "PATH")))
+(add-to-list 'exec-path "~/.cabal/bin")
+
+;; Require common lisp
+(require 'cl)
+
+;; Require Emacs package functionality
+(require 'package)
+
+;; Add MELPA and Marmalade repository to the list of package sources
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;;(setq package-archive-enable-alist '(("melpa" deft magit)))
+
+;; Initialise the package system
+(package-initialize)
+
+;; Define default packages
+(defvar henrik/packages '( graphene
+                           tuareg
+                           ;; haskell part
+                           haskell-mode
+                           shm
+                           ghc
+                           company-ghc
+                           ;; misc
+                           org
+                           solarized-theme)
+  "My default packages.")
+
+;; Load default packages at startup
+(defun henrik/packages-installed-p ()
+  "Try to install packages."
+  (loop for pkg in henrik/packages
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
+
+(unless (henrik/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg henrik/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+;; Load graphene for a clean setup and sane defaults
+(require 'graphene)
+
+;; Disable menu
+(menu-bar-mode -1)
+
+;; Load solarized theme
+(if window-system
+    (load-theme 'solarized-light t)
+  (load-theme 'wombat))
+
+;; Use wind move for more natural window switching
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
+
+;; Use the system clipboard
+(setq x-select-enable-clipboard t)
+
+;; Set key bindings
+(global-set-key (kbd "M-SPC") 'company-complete)
+
+;; Customize Tuareg
+(setq tuareg-lazy-= t)
+(setq tuareg-lazy-paren t)
+(setq tuareg-in-indent 0)
+(add-hook 'tuareg-mode-hook
+          ;; Turn on autofill minor mode.
+          (lambda () (auto-fill-mode 1)))
+
+;; Config for Haskell
+(add-hook 'haskell-mode-hook 'structured-haskell-mode)
+(eval-after-load 'haskell-mode
+  '(define-key haskell-mode-map [f8] 'haskell-navigate-imports))
+(custom-set-variables '(haskell-tags-on-save t))
+
+;; Haskell key bindings
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t))
+  ;;'(haskell-process-type 'cabal-repl))
+(eval-after-load 'haskell-mode '(progn
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
+  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+  (define-key haskell-mode-map (kbd "C-c C-o") 'haskell-compile)))
+(eval-after-load 'haskell-cabal '(progn
+  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-o") 'haskell-compile)))
+
+;; Enable ghc-mod
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+
+;; Enable GHC-Mod as Company back end
+(add-to-list 'company-backends 'company-ghc)
+(custom-set-variables '(company-ghc-show-info t))
+
+(provide 'init)
+
+;;; init.el ends here
